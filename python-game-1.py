3bff6e94cf51af5d5a11356604a339417bca6cd9
wojciechmialkowski <woj.mialkowski@gmail.com>
2022-06-02 15:05:21 +0200

    rock,paper,scissors game - correction

--- python-game-1.py
+++ python-game-1.py
@@ -1,15 +1,18 @@
 from random import randint
 
+
 def get_computers_choice():
-    random_number = randint(1,3)
+    random_number = randint(1, 3)
     options = {1: 'rock', 2: 'paper', 3: 'scissors'}
     computers_choice = options[random_number]
     return computers_choice
+
 
 def get_user_input():
     user_input = input('Enter rock/paper/scissors: ')
     user_input = user_input.lower()
     return user_input
+
 
 def get_result(user_pick, computer_pick):
     if computer_pick == user_pick:
@@ -22,6 +25,7 @@ def get_result(user_pick, computer_pick):
         return 'WIN'
     else:
         return 'LOSE'
+
 
 computer_pick = get_computers_choice()
 while True:
